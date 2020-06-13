762c6bb4e88def877c2a7d501a41b23e638446f7
AlekzandrM <mishenko.aleksandr@gmail.com>
2020-06-13 13:43:05 +0300

    Created App, bank-card.component, reset css.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BankCardComponent } from './bank-card/bank-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BankCardComponent
   ],
   imports: [
     BrowserModule,
